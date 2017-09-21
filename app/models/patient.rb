f5ff5f8fb53245485198cb950291c21a91fff2f8
Christopheraarguello <christopheraarguello@gmail.com>
2017-09-21 17:57:44 -0400

    added bmi and bmi status to models

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -15,7 +15,46 @@ class Patient < ApplicationRecord
   end
 
   def bmi
-    ((weight/((height * 12.0)*(height * 12.0))) * 703).round
+    return ((weight/((height * 12.0)*(height * 12.0))) * 703).round
   end
+
+  def bmi_status
+    case bmi
+    when (0..18)
+      "Underweight"
+    when (19..24)
+      "Normal"
+    when (25..29)
+      "Overweight"
+    when (30..39)
+      "Obese"
+    when (40..54)
+      "Extremely Obese"
+    end
+  end
+
+
+    # if bmi < 19
+    #   "Underweight"
+    # elsif @bmi >= 19 && <=24
+    #   "normal"
+    #
+    # elsif @bmi >= 54
+    #   "ERROR"
+    # end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,3 +1,19 @@
 class Weight < ApplicationRecord
   belongs_to :patient
+
+  def avg_weight
+    weight.average(:weight)
+  end
+
+  def min_weight
+    # Patient.first.weights.minimum(:weight)
+    weight.minimum(:weight)
+  end
+
+  def max_weight
+    # Patient.first.weights.maximum(:weight)
+    weight.maximum(:weight)
+  end
+
+
 end
